20fc9119a04e5441df58e995f0c03354a7f356b5
Cendos88 <cendos88@gmail.com>
2023-11-01 14:24:39 +0100

    first commit

---
+++ GeorgeWannaBe/src/main/java/com/myapp/georgewannabe/GeorgeWannaBeApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.georgewannabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeorgeWannaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeorgeWannaBeApplication.class, args);
+    }
+
+}
---
+++ GeorgeWannaBe/src/test/java/com/myapp/georgewannabe/GeorgeWannaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.georgewannabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeorgeWannaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
